15e8f0e2a744401c172fa3fbb4d38884b95b4ce5
Akshay Meher <akshay_111meher@hotmail.com>
2023-12-08 22:51:59 +0400

    Listen inbox events

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod parser;
 
+use std::thread;
+use tokio::runtime::Runtime;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-    let _ = parser::parse().await;
+    let parser_handle_1 = thread::spawn({
+        move || {
+            let rt = Runtime::new().unwrap();
+            rt.block_on(async move {
+                let _ = parser::parse(1, 2).await;
+            });
+        }
+    });
 
+    parser_handle_1.join().unwrap();
     Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,83 @@
-pub async fn parse() -> Result<(), Box<dyn std::error::Error>> {
-    todo!("To Do")
+use bindings::outbox::SentMessageFilter;
+use ethers::prelude::*;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+use std::{str::FromStr, sync::Arc};
+
+lazy_static! {
+    static ref CHAIN_HASHMAP: HashMap<u64, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(1, "ethereum wss url");
+        m.insert(2, "ropsten wss url");
+        m
+    };
+}
+
+lazy_static! {
+    static ref INBOX_CONTRACT: HashMap<u64, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(1, "ethereum inbox address");
+        m.insert(2, "ropsten inbox address");
+        m
+    };
+}
+
+lazy_static! {
+    static ref OUTBOX_CONTRACT: HashMap<u64, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(1, "ethereum OUTBOX address");
+        m.insert(2, "ropsten OUTBOX address");
+        m
+    };
+}
+
+pub async fn parse(from_chain_id: u64, to_chain_id: u64) -> Result<(), Box<dyn std::error::Error>> {
+    let from_wss = CHAIN_HASHMAP
+        .get(&from_chain_id)
+        .ok_or_else(|| "From chain ID not found in the hashmap")?;
+    let to_wss = CHAIN_HASHMAP
+        .get(&to_chain_id)
+        .ok_or_else(|| "To chain ID not found in the hashmap")?;
+
+    let inbox_contract = INBOX_CONTRACT
+        .get(&from_chain_id)
+        .ok_or_else(|| "From chain ID not found in the hashmap")?;
+    let outbox_contract = OUTBOX_CONTRACT
+        .get(&from_chain_id)
+        .ok_or_else(|| "From chain ID not found in the hashmap")?;
+
+    // Example operation: print the RPC URLs
+    println!("From WSS URL: {}", from_wss);
+    println!("To WSS URL: {}", to_wss);
+
+    let key = "0xca84b7b947ced9b5476ea2ed1605e3bb28f867e79807ae134926fa6242cfaf2d"; //enclave does not support env directly, hence adding it directly for now
+
+    let from_signer = key
+        .parse::<LocalWallet>()
+        .unwrap()
+        .with_chain_id(from_chain_id);
+    let to_signer = key
+        .parse::<LocalWallet>()
+        .unwrap()
+        .with_chain_id(to_chain_id);
+
+    let from_provider_ws = Provider::<Ws>::connect_with_reconnects(&from_wss, 2)
+        .await?
+        .with_signer(from_signer);
+
+    let to_provider_ws = Provider::<Ws>::connect_with_reconnects(&to_wss, 2)
+        .await?
+        .with_signer(to_signer);
+
+    let from_client = Arc::new(from_provider_ws);
+
+    let inbox_contract_address = Address::from_str(inbox_contract)?;
+
+    let event = Contract::event_of_type::<SentMessageFilter>(Arc::clone(&from_client))
+        .address(ValueOrArray::Array(vec![inbox_contract_address]));
+
+    let mut stream = event.subscribe_with_meta().await?;
+
+    while let Some(Ok((event, meta))) = stream.next().await {}
+    Ok(())
 }
